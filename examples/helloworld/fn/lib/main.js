f65f3703e1db4ee77b36c70df1b7eacbfe283136
Martin Feineis <mfeineis@users.noreply.github.com>
2019-06-30 00:07:22 +0200

    feat: Make `fn/lib/main` configurable handing in handlers and configuration
    The config is pretty rudimentary right now, only supporting `before` and
    `after` middleware slots. We accumulate a lot of logic inside the node runtime
    right now, maybe we should move most of it into a binary?

--- examples/helloworld/fn/lib/main.js
+++ examples/helloworld/fn/lib/main.js
@@ -1,79 +1,103 @@
-const { bodyAsJson, collapse, extractBody, utils } = require("./lib.js");
-const handlers = require("../../index.js");
-const { factory: log } = require("./loggingMiddleware.js");
-
+const { bodyAsJson, collapse, extractBody, initialize, utils } = require("./lib.js");
+const { factory: loggingMiddleware } = require("./loggingMiddleware.js");
 const { trace } = utils;
 
-const endpointsByName = {};
-const endpoints = [];
-Object.keys(handlers).forEach(function (name) {
-    const endpoint = utils.pipe([
-        extractBody,
-        bodyAsJson,
-        log("before"),
-        handlers[name],
-        log("after"),
-        collapse,
-    ]);
-    endpointsByName[utils.normalizeEndpointName(name)] = endpoint;
-    endpoints.push(endpoint);
-});
+const builtins = {
+    bodyAsJson: bodyAsJson,
+    extractBody: extractBody,
+    loggingMiddleware: loggingMiddleware,
+};
 
-trace("Endpoints detected", endpointsByName);
-
-function selectEndpoint(route) {
-    trace("selectEndpoint", "Route: ", route);
-
-    const pathname = utils.normalizeEndpointName(route.pathname);
-    if (pathname in endpointsByName) {
-        trace("  selected specific endpoint", pathname);
-        return endpointsByName[pathname];
+function createMiddleware(middleware) {
+    if (typeof middleware === "string") {
+        return builtins[middleware];
     }
-
-    // FIXME: Actually select the default endpoint based on some convention
-    const defaultEndpoint = endpoints[0];
-    trace("  selected default endpoint", defaultEndpoint);
-    return defaultEndpoint;
+    return builtins[middleware[0]](middleware[1]);
 }
 
-function main(req, res) {
+const defaultConfig = {
+    "before": [
+        "extractBody",
+        "bodyAsJson",
+    ],
+};
 
-    const stdout = {
-        _buffer: [],
-        write: function write(chunk) {
-            stdout._buffer.push(chunk);
-        },
-    };
+function configureMain(handlers, config) {
+    const endpointsByName = {};
+    const endpoints = [];
+    Object.keys(handlers).forEach(function (name) {
+        const thisConfig = config[name] || defaultConfig;
+        trace("Endpoint", name, "config", thisConfig);
 
-    const stderr = {
-        _buffer: [],
-        write: function write(chunk) {
-            stderr._buffer.push(chunk);
-        },
-    };
+        const endpoint = utils.pipe([
+            [initialize],
+            (thisConfig.before || []).map(createMiddleware),
+            [handlers[name]],
+            (thisConfig.after || []).map(createMiddleware),
+            [collapse],
+        ].reduce(function (a, b) {
+            return a.concat(b);
+        }, []));
+        endpointsByName[utils.normalizeEndpointName(name)] = endpoint;
+        endpoints.push(endpoint);
+    });
 
-    function sentinel(ctx, next, stdout, stdin, stderr) {
-        if (stderr._buffer.length) {
-            // console.log("ctx", ctx, "\nstderr\n", stderr._buffer.join("")); //, next, stdout, stdin);
-            // TODO: Use problem.json?
-            const status = 400;
-            res.writeHead(status, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-                error: stderr._buffer.join(""),
-                status: 400,
-            }));
-            return;
+    trace("Endpoints detected", endpointsByName);
+
+    function selectEndpoint(route) {
+        trace("selectEndpoint", "Route: ", route);
+
+        const pathname = utils.normalizeEndpointName(route.pathname);
+        if (pathname in endpointsByName) {
+            trace("  selected specific endpoint", pathname);
+            return endpointsByName[pathname];
         }
-        //console.log("ctx", ctx, "\nstdout\n", stdout._buffer.join("")); //, next, stdout, stdin);
-        res.writeHead(200, ctx.headers);
-        res.end(stdout._buffer.join(""));
+
+        // FIXME: Actually select the default endpoint based on some convention
+        const defaultEndpoint = endpoints[0];
+        trace("  selected default endpoint", defaultEndpoint);
+        return defaultEndpoint;
     }
 
-    selectEndpoint(utils.extractRoute(req))({}, sentinel, stdout, req, stderr);
+    return function main(req, res) {
+
+        const stdout = {
+            _buffer: [],
+            write: function write(chunk) {
+                stdout._buffer.push(chunk);
+            },
+        };
+
+        const stderr = {
+            _buffer: [],
+            write: function write(chunk) {
+                stderr._buffer.push(chunk);
+            },
+        };
+
+        function sentinel(ctx, next, stdout, stdin, stderr) {
+            if (stderr._buffer.length) {
+                // console.log("ctx", ctx, "\nstderr\n", stderr._buffer.join("")); //, next, stdout, stdin);
+                // TODO: Use problem.json?
+                const status = 400;
+                res.writeHead(status, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({
+                    error: stderr._buffer.join(""),
+                    status: status,
+                }));
+                return;
+            }
+            //console.log("ctx", ctx, "\nstdout\n", stdout._buffer.join("")); //, next, stdout, stdin);
+            res.writeHead(200, ctx.headers);
+            res.end(stdout._buffer.join(""));
+        }
+
+        selectEndpoint(utils.extractRoute(req))({}, sentinel, stdout, req, stderr);
+    };
 }
 
 module.exports = {
-    main: main,
+    configureMain: configureMain,
     trace: trace,
 };
 
